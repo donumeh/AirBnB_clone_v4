b49a9725e7986ae58cdb7bf31d206a09e285f983
Donald <donumeh54@gmail.com>
2024-08-10 17:54:21 +0100

    jquery for getting json from api

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,41 @@
+// On load update the amenities search filters
+$(document).ready(
+
+    function () {
+        let selectedAmenities = {};
+
+        $('input[type=checkbox]').on('click', function () {
+            const amenityId = $(this).attr('data-id');
+            const amenityName = $(this).attr('data-name');
+
+            if ($(this).is(':checked')) {
+                selectedAmenities[amenityId] = amenityName;
+            } else {
+                delete selectedAmenities[amenityId];
+            }
+
+            updateAmenities();
+        });
+
+        function updateAmenities() {
+            const amenitiesList = Object.values(selectedAmenities).join(', ');
+            $('.amenities h4').text(amenitiesList);
+        }
+
+        $.ajax({
+            url: 'http://0.0.0.0:5001/api/v1/status/',
+            success: function (result) {
+
+                if (result.status === 'OK') {
+                    $('#api_status').addClass('available');
+                } else {
+                    $('#api_status').removeClass('available');
+                }
+            
+            },
+            error: function (xhr, status, error) {
+                $('#api_status').removeClass('available');
+            }
+        });
+    }
+);
