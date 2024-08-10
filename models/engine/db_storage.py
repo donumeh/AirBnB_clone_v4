06c1c0f42351facc0a529de47aa7a28b0b0609de
Donald <donumeh54@gmail.com>
2024-08-10 17:20:25 +0100

    Updated the api cors, amenities filter, api status

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -15,6 +15,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from urllib.parse import quote
 
 classes = {"Amenity": Amenity, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
@@ -34,7 +35,7 @@ class DBStorage:
         HBNB_ENV = getenv('HBNB_ENV')
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
                                       format(HBNB_MYSQL_USER,
-                                             HBNB_MYSQL_PWD,
+                                             quote(HBNB_MYSQL_PWD),
                                              HBNB_MYSQL_HOST,
                                              HBNB_MYSQL_DB))
         if HBNB_ENV == "test":
