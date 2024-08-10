48c683b3e6f3d8ce572d18d8f8a2cf0a67e1cf59
Donald <donumeh54@gmail.com>
2024-08-10 17:40:03 +0100

    updated javascript to listen to api and updated cors in api code

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -21,5 +21,21 @@ $(document).ready(
             const amenitiesList = Object.values(selectedAmenities).join(', ');
             $('.amenities h4').text(amenitiesList);
         }
+
+        $.ajax({
+            url: 'http://0.0.0.0:5001/api/v1/status/',
+            success: function (result) {
+
+                if (result.status === 'OK') {
+                    $('#api_status').addClass('available');
+                } else {
+                    $('#api_status').removeClass('available');
+                }
+            
+            },
+            error: function (xhr, status, error) {
+                $('#api_status').removeClass('available');
+            }
+        });
     }
 );
